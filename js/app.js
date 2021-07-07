3f64907bd027e771083226ad915458b657b83a86
suhaibokour <okoursuhaib@gmail.com>
2021-07-07 15:25:54 +0300

    function4

--- js/app.js
+++ js/app.js
@@ -1,64 +1,65 @@
-'use strict';  
+'use strict';
 
-function question1(){
-let username = prompt('what is your name');
-alert('helle ' + username + ' lit us play a game');
-let score = 0
-let contry = prompt('do you think i am from jordan?(yes(y) or no(n))');
-contry = contry.toLowerCase();
-if (contry === 'yes' || contry === 'y') {
-  alert('you are true, i am from jordan');
-  score++
-  // console.log('you are true, i am from jordan');
-} else {
-  alert('no,i am form jordan');
-  // console.log('no,i am form jordan');
-}
+function question1() {
+  let username = prompt('what is your name');
+  alert('helle ' + username + ' lit us play a game');
+  let score = 0
+  let contry = prompt('do you think i am from jordan?(yes(y) or no(n))');
+  contry = contry.toLowerCase();
+  if (contry === 'yes' || contry === 'y') {
+    alert('you are true, i am from jordan');
+    score++
+    // console.log('you are true, i am from jordan');
+  } else {
+    alert('no,i am form jordan');
+    // console.log('no,i am form jordan');
+  }
 }
 question1();
 
-function question2(){
-let study = prompt('do you think i am a civil engineer?(yes(y) or no(n))');
-study = study.toLowerCase();
-if (study === 'yes' || study === 'y') {
-  alert('you are true, i am a civil engineer');
-  // console.log('you are true, i am a civil engineer');
-  score++
-} else {
-  alert('no,i am a civil engineer');
-  // console.log('no,i am a civil engineer');
-}
+function question2() {
+  let study = prompt('do you think i am a civil engineer?(yes(y) or no(n))');
+  study = study.toLowerCase();
+  if (study === 'yes' || study === 'y') {
+    alert('you are true, i am a civil engineer');
+    // console.log('you are true, i am a civil engineer');
+    score++
+  } else {
+    alert('no,i am a civil engineer');
+    // console.log('no,i am a civil engineer');
+  }
 }
 question2();
 
-function question3(){
-let work = prompt('do you think i worked as a civil engineer?(yes(y) or no(n))');
-work = work.toLowerCase();
-if (work === 'yes' || work === 'y') {
-  alert('you are true, i worked as civil engineer');
-  // console.log('you are true, i worked as civil engineer');
-  score++
-} else {
-  alert('no,i worked as civil engineer');
-  // console.log('no,i worked as civil engineer');
-}
+function question3() {
+  let work = prompt('do you think i worked as a civil engineer?(yes(y) or no(n))');
+  work = work.toLowerCase();
+  if (work === 'yes' || work === 'y') {
+    alert('you are true, i worked as civil engineer');
+    // console.log('you are true, i worked as civil engineer');
+    score++
+  } else {
+    alert('no,i worked as civil engineer');
+    // console.log('no,i worked as civil engineer');
+  }
 }
 question3();
- 
-let years = prompt('do you think i worked 4 years as a civil engineer?(yes(y) or no(n))');
-years = years.toLowerCase();
-if (years === 'yes' || years === 'y') {
-  alert('you are true, i worked as civil engineer 4 years');
-  // console.log('you are true, i worked as civil engineer 4 years');
-  score++
-} else {
-  alert('no,i worked as civil engineer 4 years');
-  console.log('no,i worked as civil engineer 4 years');
+function question4() {
+  let years = prompt('do you think i worked 4 years as a civil engineer?(yes(y) or no(n))');
+  years = years.toLowerCase();
+  if (years === 'yes' || years === 'y') {
+    alert('you are true, i worked as civil engineer 4 years');
+    // console.log('you are true, i worked as civil engineer 4 years');
+    score++
+  } else {
+    alert('no,i worked as civil engineer 4 years');
+    console.log('no,i worked as civil engineer 4 years');
+  }
+
+
+  alert('thank you ' + username + ' I\'m glad you\'re here');
 }
-
-
-alert('thank you ' + username + ' I\'m glad you\'re here');
-
+question4();
 
 
 
